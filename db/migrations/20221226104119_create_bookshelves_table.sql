6a6a7f65b5d79d753ce822dceb3734aaa10495d2
JINJOO PARK <lullu@JINJOOui-MacBookPro.local>
2022-12-28 14:27:08 +0900

    [40기 박진주] database 수정 : payments 테이블 review 테이블

--- db/migrations/20221226104119_create_bookshelves_table.sql
+++ db/migrations/20221226104119_create_bookshelves_table.sql
@@ -2,7 +2,10 @@
 CREATE TABLE bookshelves (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
-    CONSTRAINT bookshelves_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id)
+    book_id INT NOT NULL,
+    online_price decimal(10,2) NOT NULL,
+    CONSTRAINT bookshelves_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT bookshelves_book_id_fkey FOREIGN KEY (book_id) REFERENCES books(id)
 )
 -- migrate:down
 DROP TABLE bookshelves;
--- db/migrations/20221226104121_create_order_items_table.sql
+++ db/migrations/20221226104121_create_order_items_table.sql
@@ -1,17 +1,11 @@
 -- migrate:up
 CREATE TABLE order_items (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    user_id INT NOT NULL,
-    book_id INT NOT NULL,
     bookshelf_id INT NOT NULL,
-    order_status_id INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP NULL,
-    CONSTRAINT order_items_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT order_items_book_id_fkey FOREIGN KEY (book_id) REFERENCES books(id),
-    CONSTRAINT order_items_bookshelf_id_fkey FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id),
-    CONSTRAINT order_items_order_status_id_fkey FOREIGN KEY (order_status_id) REFERENCES order_statuses(id)
+    CONSTRAINT order_items_bookshelf_id_fkey FOREIGN KEY (bookshelf_id) REFERENCES bookshelves(id)
 )
 -- migrate:down
 DROP TABLE order_items;
